2b30371c2a5bf3acc9592eb657e977cc8c414fe9
242darkman <bvouvou@gmail.com>
2023-06-21 14:17:35 +0200

    feat: ajout d'un fichier `.env` pour fixer les variables d'environnement du projet

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv';
 import express from 'express';
 
+dotenv.config();
+
 const app = express();
-const port = 3200;
+const port = process.env.PORT || 3020;
 
 app.listen(port);
