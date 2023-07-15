af07226b82e331734cdc019d4d50b732d0eec350
242darkman <bvouvou@gmail.com>
2023-07-15 22:25:26 +0200

    feat: trier les employées par salaire en ordre décroissant

--- exercices/exobook.js
+++ exercices/exobook.js
@@ -3,3 +3,6 @@ db.employees.find({});
 
 // 2. Trouver tous les documents où l'âge est supérieur à 33.
 db.employees.find({ age: { $gt: 33 } });
+
+// 3. Trier les documents dans la collection "employees" par salaire décroissant.
+db.employees.find({}).sort({ salary: -1 });
