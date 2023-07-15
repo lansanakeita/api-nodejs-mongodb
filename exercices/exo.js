4ba3c88c80180d508244f9743632dbb3bef2bbe2
242darkman <bvouvou@gmail.com>
2023-07-15 15:35:51 +0200

    feat: requete pour récupérer le nom des salles qui possèdent une capacité d’accueil strictement supérieure à 1000 places.

--- exercices/exo.js
+++ exercices/exo.js
@@ -1,2 +1,5 @@
 // 1. Affichez l’identifiant et le nom des salles qui sont des SMAC.
 db.salles.find({ smac: true }, { _id: 1, nom: 1 });
+
+// 2. Affichez le nom des salles qui possèdent une capacité d’accueil strictement supérieure à 1000 places.
+db.salles.find({ capacite: { $gt: 1000 } }, { _id: 0, nom: 1 });
