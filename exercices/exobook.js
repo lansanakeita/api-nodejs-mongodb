f1ef6755af6d4f42e47564bd6f839b14558cda53
242darkman <bvouvou@gmail.com>
2023-07-15 22:18:37 +0200

    feat: récupérer tous les documents où l'âge est supérieur à 33.

--- exercices/exobook.js
+++ exercices/exobook.js
@@ -1,2 +1,5 @@
 // 1. Trouver tous les documents dans la collection "employees"
 db.employees.find({});
+
+// 2. Trouver tous les documents où l'âge est supérieur à 33.
+db.employees.find({ age: { $gt: 33 } });
