12186cda7ba117afc37f2ea6e25462c3b3f7645e
242darkman <bvouvou@gmail.com>
2023-07-15 15:44:18 +0200

    feat: récupérer l’identifiant puis le nom des salles qui ont exactement un avis

--- exercices/exo.js
+++ exercices/exo.js
@@ -6,3 +6,6 @@ db.salles.find({ capacite: { $gt: 1000 } }, { _id: 0, nom: 1 });
 
 // 3. Affichez l’identifiant des salles pour lesquelles le champ adresse ne comporte pas de numéro.
 db.salles.find({ 'adresse.numero': { $exists: false } }, { _id: 1 });
+
+// 4. Affichez l’identifiant puis le nom des salles qui ont exactement un avis.
+db.salles.find({ avis: { $size: 1 } }, { _id: 1, nom: 1 });
