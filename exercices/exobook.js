af26dcdd7ed165cc7e9ea2084d3c278b3698a659
242darkman <bvouvou@gmail.com>
2023-07-15 22:28:31 +0200

    feat: sélectionner uniquement le nom et le job de chaque document.

--- exercices/exobook.js
+++ exercices/exobook.js
@@ -6,3 +6,6 @@ db.employees.find({ age: { $gt: 33 } });
 
 // 3. Trier les documents dans la collection "employees" par salaire décroissant.
 db.employees.find({}).sort({ salary: -1 });
+
+// 4. Sélectionner uniquement le nom et le job de chaque document.
+db.employees.find({}, { name: 1, job: 1, _id: 0 });
