dd08d2b5e2a8e72fbf843b80fd336ae22f4c6c24
242darkman <bvouvou@gmail.com>
2023-07-15 15:38:19 +0200

    feat: requete pour récupérer l’identifiant des salles pour lesquelles le champ adresse ne comporte pas de numéro

--- exercices/exo.js
+++ exercices/exo.js
@@ -3,3 +3,6 @@ db.salles.find({ smac: true }, { _id: 1, nom: 1 });
 
 // 2. Affichez le nom des salles qui possèdent une capacité d’accueil strictement supérieure à 1000 places.
 db.salles.find({ capacite: { $gt: 1000 } }, { _id: 0, nom: 1 });
+
+// 3. Affichez l’identifiant des salles pour lesquelles le champ adresse ne comporte pas de numéro.
+db.salles.find({ 'adresse.numero': { $exists: false } }, { _id: 1 });
