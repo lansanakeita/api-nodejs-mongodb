3f0e08f1d8edd329f574053ca5a4e42c45380d2e
lansanakeita <lansanakeita1900@gmail.com>
2023-07-25 09:25:01 +0200

    feat : ajout des requetes

--- src/routes/order/orderRoutes.js
+++ src/routes/order/orderRoutes.js
@@ -10,6 +10,33 @@ import express from 'express';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /order:
+ *   post:
+ *     tags:
+ *      - Order
+ *     description: Create a new order
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userId:
+ *                 type: ObjectId
+ *               productId:
+ *                 type: ObjectId
+ *               quantity:
+ *                 type: number
+ *     responses:
+ *       201:
+ *         description: Order created successfully
+ *       400:
+ *         description: Invalid request
+ */
+
 router.post('/order', createOrder);
 
 router.get('/orders', getOrders);
