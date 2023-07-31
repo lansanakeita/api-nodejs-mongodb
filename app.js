27ca5a70d01ecf4b4c12229c686661d44442cceb
242darkman <53907600+242darkman@users.noreply.github.com>
2023-07-31 22:07:29 +0200

    feat: mise à jour du fichier suite à l'ajout d'un système de journalisation

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import loadRoutes from './src/utils/routes.loader.js';
+import logger from './src/utils/logger.js';
 import morgan from 'morgan';
+import path from 'path';
 import swaggerDocs from './src/utils/swagger.js';
 import swaggerUI from 'swagger-ui-express';
-import userRoutes from './src/routes/user/userRoutes.js';
-import ProductRoutes from './src/routes/product/productRoutes.js';
-import OrderRoutes from './src/routes/order/orderRoutes.js';
 
 const app = express();
 
-/**
- * @ express.json()
- * middleware qui parse automatiquement le corps des requêtes entrantes au format JSON.
- *  afin d'accéder à des objets JSON envoyés avec les requêtes via req.body dans vos gestionnaires de routes.
- */
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -22,8 +17,17 @@ app.use(
 );
 app.use(morgan('combined'));
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-app.use(userRoutes);
-app.use(ProductRoutes);
-app.use(OrderRoutes);
+
+await loadRoutes(path.resolve('./src/routes'), app);
+
+app.use('*', (req, res) => {
+  res.status(404).send({ error: 'Route not found' });
+});
+
+// Gestionnaire d'erreur global
+app.use((err, req, res, next) => {
+  logger.error(`Unhandled Error: ${err.message}`, err);
+  res.status(500).send({ error: 'An unexpected error occurred!' });
+});
 
 export default app;
