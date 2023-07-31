c5aaaae397852941ae8642b9249a58cdf5db31ca
242darkman <53907600+242darkman@users.noreply.github.com>
2023-07-31 22:56:29 +0200

    feta: ajout des infos de journalisation dans la console avec winston

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,12 +2,14 @@ import { createLogger, format, transports } from 'winston';
 
 const logger = createLogger({
   format: format.combine(
+    format.colorize(),
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
   ),
   transports: [
     new transports.File({ filename: 'error.log', level: 'error' }),
     new transports.File({ filename: 'combined.log' }),
+    new transports.Console(),
   ],
 });
 
