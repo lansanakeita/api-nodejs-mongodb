122024b5a585cfbb9d8199db5d0240252107facb
242darkman <53907600+242darkman@users.noreply.github.com>
2023-08-01 20:50:56 +0200

    test: ajout d'un test unitaire pour le usre controller

--- src/tests/user/user.spec.js
+++ src/tests/user/user.spec.js
@@ -1,22 +1,68 @@
-import request from 'supertest';
-import app from '../../app';
-import User from '../../models/user';
-import { after } from 'lodash';
+import {
+  createUser,
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from '../../controllers/user/user.controller.js';
+
+import User from '../../models/user/user.model.js';
+import { userDTO } from '../../models/user/dto/userDTO.js';
+import { validationResult } from 'express-validator';
+
+jest.mock('../../models/user/user.model');
+jest.mock('express-validator');
 
 describe('User Controller', () => {
-  let user;
-
-  beforeEach(async () => {
-    await User.deleteMany({});
+  beforeEach(() => {
+    jest.resetAllMocks();
   });
 
-  afterEach(async () => {});
+  describe('getUsers', () => {
+    it('should get all users', async () => {
+      const req = {};
+      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+      User.find.mockResolvedValue([{}, {}]);
+      await getUsers(req, res);
 
-  describe('GET /users', () => {
-    it('should return all users', async () => {
-      const res = await request(app).get('/users');
-      expect(res.statusCode).toBe(200);
-      expect(res.body.length).toBeGreaterThan(0);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith(expect.any(Array));
+    });
+
+    it('should handle error when getting all users', async () => {
+      const req = {};
+      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+      User.find.mockRejectedValue(new Error('An error occurred'));
+      await getUsers(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({
+        error: 'Error: An error occurred',
+      });
+    });
+  });
+
+  describe('getUser', () => {
+    it('should get a user by id', async () => {
+      const req = { params: { id: '1' } };
+      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+      User.findById.mockResolvedValue({});
+      await getUser(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith(userDTO({}));
+    });
+
+    it('should handle error when getting a user by id', async () => {
+      const req = { params: { id: '1' } };
+      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+      User.findById.mockRejectedValue(new Error('An error occurred'));
+      await getUser(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({
+        error: 'Error: An error occurred',
+      });
     });
   });
 });
